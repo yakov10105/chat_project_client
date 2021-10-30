05c6cb3de9755017c7a517cb32681bf9689d11b5
Yakov Kantor <80357569+yakov10105@users.noreply.github.com>
2021-10-30 18:21:39 +0300

    styling

---
+++ src/components/chat/Chat.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Chat = () => {
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Chat
