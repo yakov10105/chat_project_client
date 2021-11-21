3a10cb398345108ac901e1185ae3e378c4fa5ccb
Yakov Kantor <80357569+yakov10105@users.noreply.github.com>
2021-11-21 23:36:14 +0200

    h

--- src/components/GameManager/Board/Board.js
+++ src/components/GameManager/Board/Board.js
@@ -1,5 +1,5 @@
-import React,{useEffect, useState} from 'react'
-import Data from './Fields.json'
+import React,{useEffect, useState,useContext} from 'react'
+import {Grid , Divider , TextField , List,ListItem,ListItemIcon , ListItemText,Avatar,Button, Alert } from '@mui/material'
 import Checker from '../Checker/Checker'
 import Triangle from './Triangle/Triangle'
 import getCheckers from '../getCheckers/getCheckers'
